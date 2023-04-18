6cae52ba9f15cb5ca5d000228201df1407bc17ae
DragosStoican <dragos.stoican28@gmail.com>
2023-04-18 20:03:22 +0100

    data entry diss

---
+++ temp.py
@@ -0,0 +1,54 @@
+import numpy as np
+from data_processing import get_data
+
+# %%
+arr = np.genfromtxt('data/york/traj.csv', delimiter=',')
+
+# print(arr)
+
+# arr = np.flip(arr, axis=0)
+
+
+
+
+# y = arr[::2]
+# x = arr[1::2]
+
+
+
+# arr = np.dstack((x, y))
+# arr = arr.flatten()
+
+np.savetxt('test.csv', arr)
+
+# %%
+for j in range(0, len(arr), 2):
+    arr[j] = (450 - arr[j]) % 450
+# %%
+# normal_data, abnormal_data, real_abnormal_data, real_abnormal_data_2 = get_data('york', scale=False)
+#
+# for i in range(len(normal_data)):
+#     for j in range(2, len(normal_data[i][1:])+1, 2):
+#         if j % 2 == 0:
+#             normal_data[i, j] = (450 - normal_data[i, j]) % 450
+#
+# np.savetxt('test.csv', normal_data, delimiter=',')
+
+# %%
+normal_data, abnormal_data, real_abnormal_data, real_abnormal_data_2 = get_data('york', scale=False)
+
+arr = normal_data
+
+arr = np.flip(arr, axis=1)
+
+for i, a in enumerate(arr):
+    y = a[::2]
+    x = a[1::2]
+
+    tmp = np.dstack((x, y))
+    tmp = tmp.flatten()
+    arr[i] = tmp
+
+np.savetxt('test.csv', arr, delimiter=',')
+
+
--- trajectory_viewer_york.py
+++ trajectory_viewer_york.py
@@ -15,10 +15,10 @@ def show_trajectories(image, trajectories, color):
     # Plot trajectory data
     for i, trajectory in enumerate(trajectories):
         print(f"Plotting trajectory {i + 1}/{len(trajectories)}")
-        trajectory_data = trajectory[1:]  # Remove category and label
+        trajectory_data = trajectory[1:]  # Remove category
         category = int(trajectory[0])
-        x_positions = trajectory_data[0::4]
-        y_positions = trajectory_data[1::4]
+        x_positions = trajectory_data[0::2]
+        y_positions = trajectory_data[1::2]
 
         ax.plot(x_positions, y_positions, '-', linewidth=1, color=color[category])
         ax.arrow(x_positions[-2], y_positions[-2],
@@ -31,12 +31,18 @@ def show_trajectories(image, trajectories, color):
 
 
 # %%
-dataset = 'sherbrooke'
+dataset = 'york'
 image = 'data/images/' + dataset + '.png'
 normal_data, abnormal_data, real_abnormal_data, real_abnormal_data_2 = get_data(dataset, scale=False)
 
 # %%
-# show_trajectories(image, normal_data, ['green', 'blue', 'yellow'])
-show_trajectories(image, abnormal_data, ['green', 'blue', 'yellow'])
-show_trajectories(image, real_abnormal_data, ['green', 'blue', 'yellow'])
-show_trajectories(image, real_abnormal_data_2, ['green', 'blue', 'yellow'])
+# for i in range(len(normal_data)):
+#     for j in range(2, len(normal_data[i][1:])+1, 2):
+#         if j % 2 == 0:
+#             normal_data[i, j] = (450 - normal_data[i, j]) % 450
+
+# %%
+show_trajectories(image, normal_data, ['green', 'blue', 'yellow'])
+# show_trajectories(image, abnormal_data, ['green', 'blue', 'yellow'])
+# show_trajectories(image, real_abnormal_data, ['green', 'blue', 'yellow'])
+# show_trajectories(image, real_abnormal_data_2, ['green', 'blue', 'yellow'])
